6e857895a2c02671d52195c68adc77d105963ba8
11notes <github@11notes.ch>
2017-09-01 08:30:47 +0200

    pax note

--- alpine-docker.sh
+++ alpine-docker.sh
@@ -11,6 +11,6 @@ apk add docker \
 rc-update add docker
 rc-update add open-vm-tools boot
 
-# add alpine Java VM memory fix
+# add alpine Java VM memory fix (only for dev, use paxctl -c -m on java inside container!)
 echo -e "kernel.pax.softmode=1" \
     > /etc/sysctl.d/01-docker-java-fix.conf
