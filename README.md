fedce91b4b61294ff79221ae9c2fc94e81c62366
11notes <github@11notes.ch>
2017-09-01 08:22:17 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# alpine-docker
+
+Very simple script to prepare Alpine in a VMware environment to act as a docker host
+
+## build with
+
+* [Alpine Linux](https://alpinelinux.org/) - Alpine Linux
+* [Docker](https://www.docker.com/) - Docker Container System
---
+++ alpine-docker.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# add docker rep
+echo -e "http://dl-cdn.alpinelinux.org/alpine/edge/community" \
+    >> /etc/apk/repositories
+
+# install docker & open-vm-tools
+apk update
+apk add docker \
+        open-vm-tools
+rc-update add docker
+rc-update add open-vm-tools boot
+
+# add alpine Java VM memory fix
+echo -e "kernel.pax.softmode=1" \
+    > /etc/sysctl.d/01-docker-java-fix.conf
